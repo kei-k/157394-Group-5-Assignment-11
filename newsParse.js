1dd9b4d9a399888d9546a418cda4dcc6e5a65eda
kei-k <keikawahara1@gmail.com>
2018-10-18 16:50:20 +1300

    News Page Completed

--- newsParse.js
+++ newsParse.js
@@ -1,41 +1,16 @@
-//$.get('/news.xml')
-//	.done(function(data){
-//		data = $.parseXML(data);
-//		var list = $('#newsList');
-//		$(data)
-//		.find('article')
-//		.each(function(index, element){
-//			var field = $(element)
-//			var label = field.find('newsTitle').text()
-//			var value = field.find('newsContent').text()
-//			list
-//				.append('<li>'+label+': </li>')
-//				.append('<li>'+value+': </li>')
-//				;
-//			})
-//			;
-//	})
- // .fail(function(){
-//	alert('something went wrong!');
-//  })
-//;
-//Code Starts
-$(document).ready(function(){
-	$("#news").append("<ul></ul>");
-	$.ajax({
-		type: "GET",
-		url: "news.xml",
-		dataType: "xml",
-		success: function(xml){
-			$(xml).find('article').each(function(){
-				var sTitle = $(this).find('newsTitle').text();
-				var sPublisher = $(this).find('newsContent').text();	
-			$("<li></li>").html(sTitle + ", " + sPublisher).appendTo("#news ul");
-			});
-		},
-	error: function() {
-		alert("An error occurred while processing XML file.");
-		}
-	});
-});
-//Code Ends
+$.get('http://192.168.0.101/WEBSITE%20TESTING/news.xml')
+  .done(function(data){
+    var list = $('#newsList');$(data).find('article').each(function(index, element){
+			var xmlele = $(element)
+			var headline = xmlele.find('newsTitle').text()
+			var content = xmlele.find('newsContent').text()
+			list
+				.append('<li>'+headline+'-'+content+': </li>')
+				;
+			})
+			;
+  })
+  .fail(function(error){
+    alert(error.text());
+  })
+;
